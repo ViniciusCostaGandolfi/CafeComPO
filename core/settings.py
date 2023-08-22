cb7dee4fb23a2791afe1d0174da2f5a09b0c13a1
ViniciusCostaGandolfi <vinicostagandolfi@gmail.com>
2023-08-21 21:35:10 -0300

    teste2.py

--- core/settings.py
+++ core/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-*$5f9d)+=*&x)jq9to)i2=ertm@bab!o!3tb+74(p8qt=$#!k3
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*', 'cafecompo.up.railway.app']
+ALLOWED_HOSTS = ['cafecompo.up.railway.app', 'https://*.127.0.0.1','*']
 
 
 # Application definition
